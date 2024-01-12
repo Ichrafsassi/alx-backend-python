1be22f00ebd62c4308f12a3cfc9291a14e986ffa
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-01-12 01:41:45 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""floor module"""
+
+
+def floor(a: float) -> int:
+    """ function floor which takes a float n as argument
+    and returns the floor of the float.
+    """
+    return int(a)
