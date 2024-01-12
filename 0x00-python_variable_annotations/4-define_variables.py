76aadd49ebd15007c4892cb371a2f5e8b5759a59
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-01-12 02:12:31 +0100

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task 4 : file contain the variables annotated with types"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
